659c7060b81a9282327969132ee24fc303673c8a
Juan David Perez <juandwow@gmail.com>
2022-06-21 00:40:14 -0300

    avance, terminado pruebas, falta logs

--- routes/randomsRouter.js
+++ routes/randomsRouter.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const {fork} = require('child_process');
+const Random = require('../js/randoms');
+const random = new Random();
 
 router.get('/',(req, res) => {
     if(!isNaN(req.query.cant) || !req.query.cant){
-        const forked = fork('./js/randoms');
-        forked.on('message',(msg) => {
-            if(msg == 'start'){
-                forked.send(`${req.query.cant}`);
-            }else{
-                res.json({"Numeros Random" : msg});
-            }
-        })
+        const data = random.generarRandom(req.query.cant);
+        res.json({"Numeros Random: " : data});
+        // const forked = fork('./js/randoms');
+        // forked.on('message',(msg) => {
+        //     if(msg == 'start'){
+        //         forked.send(`${req.query.cant}`);
+        //     }else{
+        //         res.json({"Numeros Random" : msg});
+        //     }
+        // })
     }else{
         res.send({Error: 'por favor ingresar un numero'});
     }
